2a7bed91ad79e2734652697837fea612cff460f8
Ignas Kižys <ignas.kizys@tutanota.com>
2023-08-15 21:29:21 +0300

    comments

--- vector.h
+++ vector.h
@@ -19,7 +19,7 @@
     void (*reset)(struct _vector_##type * vector);                             \
     void (*shrink)(struct _vector_##type * vector);                            \
   } _vector_##type;                                                            \
-                                                                               \
+  /*adds an array entry to the end of the vector*/                             \
   void _append_##type(struct _vector_##type *vector, type element) {           \
     type *new;                                                                 \
     if (vector->length + 1 > vector->size) {                                   \
@@ -32,7 +32,7 @@
     vector->data[vector->length] = element;                                    \
     vector->length++;                                                          \
   }                                                                            \
-                                                                               \
+  /*adds an array entry to the beginning of the vector*/                       \
   void _prepend_##type(struct _vector_##type *vector, type element) {          \
     vector->size =                                                             \
         vector->length + 1 > vector->size ? vector->size *= 2 : vector->size;  \
@@ -43,7 +43,7 @@
     vector->data = new;                                                        \
     vector->length++;                                                          \
   }                                                                            \
-                                                                               \
+  /*removes the entry of a vector at pos*/                                     \
   void _remove_##type(struct _vector_##type *vector, int pos) {                \
     type *new = malloc(vector->size * sizeof(type));                           \
     type *data = (*vector).data;                                               \
@@ -54,20 +54,20 @@
     vector->data = new;                                                        \
     vector->length -= 1;                                                       \
   }                                                                            \
-                                                                               \
+  /*destroys the data of the vector*/                                          \
   void _destroy_##type(struct _vector_##type *vector) {                        \
     free(vector->data);                                                        \
     vector->length = NULL;                                                     \
     vector->size = NULL;                                                       \
   }                                                                            \
-                                                                               \
+  /*resets the vector to the default settings*/                                \
   void _reset_##type(struct _vector_##type *vector) {                          \
     free(vector->data);                                                        \
     (*vector).data = (type *)malloc(10 * sizeof(type));                        \
     (*vector).size = 10;                                                       \
     (*vector).length = 0;                                                      \
   }                                                                            \
-                                                                               \
+  /*shrinks the vector size to the length*/                                    \
   void _shrink_to_length_##type(struct _vector_##type *vector) {               \
     type *new = malloc(vector->length * sizeof(type));                         \
     memcpy(new, vector->data, vector->length * sizeof(type));                  \
@@ -75,7 +75,7 @@
     vector->data = new;                                                        \
     vector->size = vector->length;                                             \
   }                                                                            \
-                                                                               \
+  /*vector initialization*/                                                    \
   _vector_##type _new_vector_##type() {                                        \
     _vector_##type ret;                                                        \
     ret.data = (type *)malloc(10 * sizeof(type));                              \
