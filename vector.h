bace62171d1bd5245d5d93a85f0acbd5d2fda1e9
Ignas Kižys <ignas.kizys@tutanota.com>
2023-08-15 21:11:14 +0300

    fuck

--- vector.h
+++ vector.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <string.h>
-
+// this is incredibly shit
 #define define_list(type)                                                      \
   struct _vector;                                                              \
                                                                                \
